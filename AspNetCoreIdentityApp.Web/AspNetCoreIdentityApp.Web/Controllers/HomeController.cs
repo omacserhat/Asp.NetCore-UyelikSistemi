45699b710ba12c3c45f63d705845db00020cdf97
Serhat OMAÇ <omacserhatt@gmail.com>
2024-03-07 11:49:50 +0300

    Kayıt ol sayfasının tasarlanması

--- AspNetCoreIdentityApp.Web/AspNetCoreIdentityApp.Web/Controllers/HomeController.cs
+++ AspNetCoreIdentityApp.Web/AspNetCoreIdentityApp.Web/Controllers/HomeController.cs
@@ -23,6 +23,13 @@ namespace AspNetCoreIdentityApp.Web.Controllers
             return View();
         }
 
+        public IActionResult SignUp()
+        {
+
+
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
